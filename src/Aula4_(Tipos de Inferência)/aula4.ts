6ed4a509d83df3a3b7f405328e9fdf6bead31d3a
EricSampaio <erick.gtn.xp@gmail.com>
2026-04-14 17:43:45 -0300

    criação da aula 4
    Criação e detalhamento da aula 4 (inferÊncia de tipos)

---
+++ src/Aula4_(Tipos de Inferência)/aula4.ts
@@ -0,0 +1,35 @@
+//####### CONCEITO DE INFERêNCIA ########
+
+// Você não precisa escrever: let curso: string = "ADS";
+let curso = "ADS"; // O TS já inferiu que é STRING
+//curso = 10; 
+// Erro: Type 'number' is not assignable to type 'string'.
+
+
+// ######FALHA DE INFERÊNCIA######
+let disciplina; // Aqui o TS define como tipo 'any' (perigoso!)
+disciplina = "Programação II"; // Aceita
+disciplina = 2; // Também aceita (perdemos a segurança)
+
+
+//############ UNION TYPES ###############
+let veiculo:string[]=["Porta","Motor","Banco"] // só aceita strings
+
+let carro:(string|number)[]=["Pneu","Motor",1500] // aceita strings ou números
+
+
+// ############ FUNÇÕES COM UNION TYPES ###############
+function exibirId(id: number | string) {
+    console.log(`O ID do aluno é: ${id}`);
+}
+exibirId(10);      // Funciona
+exibirId("A-50");  // Funciona
+
+// ################# DESAFIO DO "TYPE NARROWING" (Estreitamento) ####################
+function processarEntrada(valor: number | string) {
+    if (typeof valor === "string") {
+        console.log(valor.toUpperCase()); // Aqui o TS sabe que é string
+    } else {
+        console.log(valor.toFixed(2)); // Aqui o TS sabe que é number
+    }
+}
